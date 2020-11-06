926d7b035b9fef8efab49be35f171d32bf8da25a
David Laing <dlaing@vmware.com>
2020-11-06 19:55:21 +0000

    Skeleton app showing issue

---
+++ awesome_pkg/test/test_thing_doer.py
@@ -0,0 +1,4 @@
+from awesome_pkg import thing_doer
+
+def test_do_work_should_do_something():
+    assert thing_doer.do_work('Flux capacitor') == "Work completed on Flux capacitor!"
---
+++ awesome_pkg/thing_doer.py
@@ -0,0 +1,2 @@
+def do_work(arg1):
+    return f"Work completed on {arg1}!" 
